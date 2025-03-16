b7cc727489bb686ccef387bc6feb90e7c4f49d9a
Rohail Rathore <96627590+Rohail30@users.noreply.github.com>
2025-03-16 17:48:34 +0500

    Update L4.sol

--- Lecture_04/L4.sol
+++ Lecture_04/L4.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.22;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MustanAli is ERC20 {
+contract Rohail is ERC20 {
 
     constructor(address recipient) ERC20("Rohail", "RR") {
         _mint(recipient, 100000 * 10**decimals());
